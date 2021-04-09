28c3c80ae40df7365a1c0e3100c555115fc3e42f
yoonsu <yoonsu2435@naver.com>
2021-04-09 15:44:42 +0900

    01day_example_complete

--- 01.py
+++ 01.py
@@ -1,3 +1,11 @@
-print("Hello World3")
-print("Hello World3")
-print("Hello World3")
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+i1 = input()
+d1 = input()
+s1 = input()
+
+print(i+int(i1))
+print(d+float(d1))
+print(s+s1)
